c76173e3c25613bd10314b76bd3100e57b9a064e
Oleg <osipaualeh@gmail.com>
2018-04-02 23:26:55 +0300

    Filter.Predicate

---
+++ NET.S.2018.Osipov.02/Filter/Filter/Filter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter
+{
+    public class Filter
+    {
+        #region Logic
+        public static int[] DigitFilter(int[] arrayofnumbers, IPredicate specifiednumber)
+        {
+            List<int> resultArray = new List<int>();
+
+            if (arrayofnumbers == null)
+            {
+                throw new ArgumentNullException("Array reference is null");
+            }
+
+            foreach (int number in arrayofnumbers)
+            {
+                if (specifiednumber.IsMatch(number))
+                    resultArray.Add(number);
+            }
+            return resultArray.ToArray();
+        }
+        #endregion
+     }
+}
---
+++ NET.S.2018.Osipov.02/Filter/Filter/Predicates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter
+{
+    public class IsContainNumber : IPredicate
+    {
+        public int specifiednumber;
+
+        public IsContainNumber(int specifiednumber)
+        {
+            this.specifiednumber = specifiednumber;
+        }
+
+        public bool IsMatch(int number)
+        {
+            while (number != 0)
+            {
+                if (number % 10 == specifiednumber) 
+                {
+                    return true;
+                }
+                number = number / 10;
+            }
+            return false;
+        }
+    } 
+}
