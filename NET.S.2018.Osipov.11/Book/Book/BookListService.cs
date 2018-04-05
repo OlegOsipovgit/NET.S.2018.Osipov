2743f76098ed29e0924bafbe3c8694aa6879e974
Oleg <osipaualeh@gmail.com>
2018-04-06 00:34:00 +0300

    books.Service

--- NET.S.2018.Osipov.11/Book/Book/BookListService.cs
+++ NET.S.2018.Osipov.11/Book/Book/BookListService.cs
@@ -8,5 +8,33 @@ namespace Book
 {
     class BookListService
     {
+        private List<Books> books = new List<Books>;
+        
+        public void AddBook(Books newbook)
+        {
+            books.Add(newbook);
+        }
+        public void RemoveBook(Books book)
+        {
+            books.Remove(book);
+        }
+        public void FindBookByTeg(Books book)
+        {
+
+        }
+        public void SortBooksByTag(IComparer<Books> comparator)
+        {
+            var booksarray = books.ToArray();
+            if (ReferenceEquals(comparator, null))
+            {
+                Array.Sort(booksarray);
+            }
+            else
+            {
+                Array.Sort(booksarray, comparator);
+            }
+            books.Clear();
+            books.AddRange(booksarray);
+        }
     }
 }
