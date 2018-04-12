3e0d99934545ae8befdea120dce484ede3827e8f
Oleg <osipaualeh@gmail.com>
2018-04-12 18:00:11 +0300

    d13

---
+++ NET.S.2018.Osipov.13/BinarySearcher/BinarySearcher/Searcher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearcher
+{
+    public static class Searcher
+    {
+        public static int Search<T>(T element, T[] array,IComparer<T> comparer)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+            int bottom = 0;
+            int top = array.Length - 1;
+            while (bottom <= top)
+            {
+
+                int mid = bottom + (top - bottom) / 2;
+                if (comparer.Compare(element,array[mid])>0) top = mid - 1;
+                else if (comparer.Compare(element, array[mid])<0) bottom = mid + 1;
+                else return mid;
+            }
+            return -1;
+        }
+}
---
+++ NET.S.2018.Osipov.13/Timer/Timer/Clock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace Timer
+{
+    public class Clock
+    {
+        private int time;
+        public Clock(int time)
+        {
+            this.time = time;
+        }
+        /// <summary>
+        /// Event
+        /// </summary>
+        public event EventHandler<ClocksFinishedEventArgs> ClocksFinished;
+        /// <summary>
+        /// Method runs event
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void OnClocksFinished(ClocksFinishedEventArgs e)
+        {
+            if (ClocksFinished != null) ClocksFinished(this,e);
+        }
+        /// <summary>
+        /// Timer method
+        /// </summary>
+        /// <param name="information"></param>
+        public void StartClocks(string information)
+        {
+            Thread.Sleep(1000 * time);
+            
+        }
+        /// <summary>
+        /// Class for information of event
+        /// </summary>
+        public sealed class ClocksFinishedEventArgs:EventArgs
+        {
+            public string Information { get; }
+            public ClocksFinishedEventArgs(string information)
+            {
+                Information = information;
+            }
+        }
+    }
+}
