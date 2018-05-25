140ce06a20fe4a5436f641f3c60159021bbc9a2a
Oleg <osipaualeh@gmail.com>
2018-05-25 12:25:57 +0300

    crossword2

---
+++ NET.W.2018.ExitTask/Crossword2/Crossword2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Crossword2
+{
+    public class Class1
+    {
+        public static string[][] arrays = new string[7][]
+         {
+          new string[]{"п","е","р","е","л","о","м"},
+         new string[]{"п","о","д","в","ы","в","и","х"},
+         new string[]{"з","а","к","р","ы","т","ы","й"},
+         new string[]{"у","ш","и","б"},
+         new string[]{"ш","и","н","а"},
+         new string[]{"в","ы","в","и","х"},
+         new string[]{"о","т","к","р","ы","т","ы","й"},
+         };
+        public static int a = 0;
+    }
+
+}
---
+++ NET.W.2018.ExitTask/Crossword2/Crossword2/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossword2
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            int temp=0;
+            string[,] crosswordfield = new string[100, 100];
+            
+            ///записываем первое слово из jaggedarray в кроссворд
+            for (int i=50;i<51;i++)
+            {
+                
+                int b = 0;
+                for (int j = 50; j < Class1.arrays[0].Length + 50; j++)
+                {
+                    crosswordfield[i, j] = Class1.arrays[Class1.a][b];
+                    b++;
+                 }
+                Class1.a++;
+            }
+            
+            ///Алгоритм поиска места для слова из jaggedarray в сетке кроссворда по совпадению букв
+            for (int i=0;i<100;i++)
+            {
+                for (int j=0;j<100;j++)
+                {
+                    for(int c=Class1.a ;c<Class1.arrays.Length;c++)
+                    {
+                        for (int b=0;b<Class1.arrays[c].Length;b++)
+                        {
+                            if (crosswordfield[i, j] == Class1.arrays[c][b])
+                            {
+                                ///проверка по вертикали
+                                if (crosswordfield[i, j - 1] == null && crosswordfield[i, j + 1] == null)
+                                {
+                                    ///Проверка на перечесчения с другими словами по вертикали
+                                    for (int q=1;q<2;q++)
+                                    {
+                                        for (int w=j-Class1.arrays[c].Length;w!=j+Class1.arrays[c].Length-b; w++)
+                                        {
+                                            for(int n=0;n<Class1.arrays.Length;n++)
+                                            {
+                                                if (crosswordfield[q, w] != null && crosswordfield[q, w] != Class1.arrays[c][n])
+                                                {
+                                                    temp++;
+                                                }
+                                            }
+                                            
+                                        }
+                                        
+                                    }
+                                    if (temp > 0) break;
+                                    for (int n = b; n < Class1.arrays[c].Length; n++)
+                                    {
+                                        crosswordfield[i, j] = Class1.arrays[c][n];
+                                        j++;
+                                    }
+                                    for (int n = b - 1; n != 0; n--)
+                                    {
+                                        crosswordfield[i, j - 1] = Class1.arrays[c][n];
+                                        j--;
+                                    }
+                                    Class1.a++;
+                                }
+                                ///проверка по горизонтали
+                                if (crosswordfield[i-1, j] == null && crosswordfield[i+1, j ] == null)
+                                {
+                                    ///Проверка на перечесчения с другими словами по горизонтали
+                                    for (int w = j - Class1.arrays[c].Length; w != j + Class1.arrays[c].Length - b; w++)
+                                    {
+                                        for (int q = 1; q < 2; q++)
+                                        {
+                                            for (int n = 0; n < Class1.arrays.Length; n++)
+                                            {
+                                                if (crosswordfield[q, w] != null && crosswordfield[q, w] != Class1.arrays[c][n])
+                                                {
+                                                    temp++;
+                                                }
+                                            }
+
+                                        }
+
+                                    }
+                                    if (temp > 0) break;
+                                    for (int n = b; n < Class1.arrays[c].Length; n++)
+                                    {
+                                        crosswordfield[i, j] = Class1.arrays[c][n];
+                                        i++;
+                                    }
+                                    for (int n = b - 1; n != 0; n--)
+                                    {
+                                        crosswordfield[i-1, j] = Class1.arrays[c][n];
+                                        i--;
+                                    }
+                                    Class1.a++;
+                                }
+                                else continue;
+                            }      
+                         }
+                        
+                     }
+                    
+                }
+            }
+           
+        }
+    }
+}
